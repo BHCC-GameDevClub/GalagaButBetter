55eb0f9c2d95b6b8c20c262cd470cc5c1c6b513a
zetsey <zetseymedia@gmail.com>
2025-09-29 00:21:39 -0400

    Updated plane swapping script

--- GalagaButBetter/Assets/Scripts/teleporter.cs
+++ GalagaButBetter/Assets/Scripts/teleporter.cs
@@ -5,13 +5,13 @@ public class Teleporter : MonoBehaviour
 
 {
     public Transform Player;
-    private float plane1 = 0f;
-    private float plane2 = 35f;
-    private bool atPlane = true;
+    private float plane1 = 0f; // Plane 1 cords
+    private float plane2 = 35f; // Plane 2 cords
+    private bool atPlane = true; // Bool to track current location
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump")) // Jump aka teleport Input
         {
             TeleportPlayer();
         }
@@ -23,16 +23,16 @@ public class Teleporter : MonoBehaviour
         {
             Vector3 currentPosition = Player.position;
             Vector3 newPosition;
-            if (atPlane)
+            if (atPlane) // position check
             {
-                newPosition = new Vector3(currentPosition.x, plane2, currentPosition.z);
+                newPosition = new Vector3(currentPosition.x, plane2, currentPosition.z); // teleport to plane2 if on plane1
             }
             else
             {
-                newPosition = new Vector3(currentPosition.x, plane1, currentPosition.z);
+                newPosition = new Vector3(currentPosition.x, plane1, currentPosition.z); // teleport to plane1 if on plane2
             }
             Player.position = newPosition;
-            atPlane = !atPlane;
+            atPlane = !atPlane; // position toggle
         }
     }
 }
